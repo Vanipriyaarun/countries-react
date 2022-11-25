c4ab03202332c759647c6d199bcedce548132993
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2022-11-25 20:00:05 +0000

    level-2 completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./App.css";
 
 function App(props) {
+  const [query, setQuery]=useState("");
   return (
-    <div className="mainCard">
-      {props.allCountry.map((country, index) => (
-        <div key={index} className="infoCard">
-          <img src={country.flag} className="card-img-top" alt="" />
-          <h2>{country.name}</h2>
-          <p><span>Population:</span> {country.population}</p>
-          <p><span>Region:</span> {country.region}</p>
-          <p><span>Capital</span> {country.capital}</p>
-        </div>
-      ))}
+    <div>
+      <input type="text" placeholder="Search for a country..." className="search" onChange={(e)=> setQuery(e.target.value)}/>
+      <div className="mainCard">
+        {props.allCountry.filter((country)=>country.name.toLowerCase().includes(query) || country.capital.toLowerCase().includes(query))
+        .map((country, index) => (
+          <div key={index} className="infoCard">
+            <img src={country.flag} className="card-img-top" alt="" />
+            <h2>{country.name}</h2>
+            <p><span>Population:</span> {country.population}</p>
+            <p><span>Region:</span> {country.region}</p>
+            <p><span>Capital</span> {country.capital}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
